b0be88b1362453546a1a82b800a3153462bfb723
Andrzej Pultyn <01178581@pw.edu.pl>
2025-07-13 17:46:35 +0200

    Tested book read

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -29,22 +29,46 @@ describe('App e2e', () => {
     beforeEach(async () => {
         prisma = app.get(PrismaService);
         await prisma.cleanDb();
-        await prisma.user.create({
-            data: {
-                id: 1,
-                email: 'admin@example.com',
-                password: await argon.hash('passwd'),
-                role: Role.ADMIN,
-            },
+
+        const admin = {
+            id: 1,
+            email: 'admin@example.com',
+            password: await argon.hash('passwd'),
+            role: Role.ADMIN,
+        };
+
+        const user = {
+            id: 2,
+            email: 'user@example.com',
+            password: await argon.hash('passwd'),
+            role: Role.USER,
+        };
+
+        const book1 = {
+            id: 1,
+            title: 'Dune',
+            author: 'Frank Herbert',
+        };
+
+        const book2 = {
+            id: 2,
+            title: 'Mistrz czystego kodu',
+            author: 'Robert C. Martin',
+        };
+
+        const review1 = {
+            id: 1,
+            bookId: 1,
+            userId: 2,
+            stars: 5,
+            comment: 'Awesome',
+        };
+
+        await prisma.user.createMany({
+            data: [admin, user],
         });
-        await prisma.user.create({
-            data: {
-                id: 2,
-                email: 'user@example.com',
-                password: await argon.hash('passwd'),
-                role: Role.USER,
-            },
-        });
+        await prisma.book.createMany({ data: [book1, book2] });
+        await prisma.review.create({ data: review1 });
     });
 
     afterAll(() => {
@@ -76,13 +100,57 @@ describe('App e2e', () => {
                     .spec()
                     .post('/api/auth/login')
                     .withBody(req)
-                    .expectStatus(200);
+                    .expectStatus(200)
+                    .expectBodyContains('access_token');
             });
         });
     });
     describe('Book', () => {
         describe('Create', () => {});
-        describe('Read', () => {});
+        describe('Read', () => {
+            it('Get list', () => {
+                return pactum
+                    .spec()
+                    .get('/api/books')
+                    .expectStatus(200)
+                    .expectJsonLength(2);
+            });
+            it('Get list with searchString', async () => {
+                await prisma.book.create({
+                    data: {
+                        id: 3,
+                        title: 'Something',
+                        author: 'Dune',
+                    },
+                });
+                return pactum
+                    .spec()
+                    .get('/api/books?searchString=dune')
+                    .expectStatus(200)
+                    .expectJsonLength(2);
+            });
+            it('Get single', () => {
+                return pactum
+                    .spec()
+                    .get('/api/books/1')
+                    .expectStatus(200)
+                    .expectBody({
+                        id: 1,
+                        title: 'Dune',
+                        author: 'Frank Herbert',
+                        reviews: [
+                            {
+                                id: 1,
+                                stars: 5,
+                                comment: 'Awesome',
+                                user: {
+                                    email: 'user@example.com',
+                                },
+                            },
+                        ],
+                    });
+            });
+        });
         describe('Update', () => {});
         describe('Delete', () => {});
     });
